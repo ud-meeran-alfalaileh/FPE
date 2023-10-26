50a5d2cb51eb821ec74fe7a473735f27c8cb7ed7
ud-meeran-alfalaileh <137434084+ud-meeran-alfalaileh@users.noreply.github.com>
2023-10-26 10:52:20 +0300

    Update calculator.dart

--- calculator.dart
+++ calculator.dart
@@ -1,35 +1,31 @@
 import 'dart:io';
+
 void main() {
-  while (true) {
-    stdout.write("Enter the first number: ");
-    double firstNum = double.parse(stdin.readLineSync()!);    // firstnum
-    stdout.write("Enter an operator (+, -, *, /): ");
-    String operator = stdin.readLineSync()!;    //operator
-    stdout.write("Enter the second number: ");
-    double secondNum = double.parse(stdin.readLineSync()!);   // secondNum
-    double result;
-    switch (operator) {
-      case '+':
-        result = firstNum + secondNum;
-        break;
-      case '-':
-        result = firstNum - secondNum;
-        break;
-      case '*':
-        result = firstNum * secondNum;
-        break;
-      case '/':
-        if (secondNum != 0) {
-          result = firstNum / secondNum;
-        } else {
-          print("Error: Division by zero is not allowed.");
-          continue;
-        }
-        break;
-      default:
-        print("Invalid operator. Please use +, -, *, or /.");
-        continue;
+  stdout.write("Enter the first number: ");
+  double firstNumber = double.parse(stdin.readLineSync().toString());
+
+  stdout.write("Enter an operator (+, -, *, /): ");
+  String operator = stdin.readLineSync().toString();
+
+  stdout.write("Enter the second number: ");
+  double secondNumber = double.parse(stdin.readLineSync().toString());
+
+  double result = 0;
+
+  if (operator == "+") {
+    result = firstNumber + secondNumber;
+  } else if (operator == "-") {
+    result = firstNumber - secondNumber;
+  } else if (operator == "*") {
+    result = firstNumber * secondNumber;
+  } else if (operator == "/") {
+    if (secondNumber != 0) {
+      result = firstNumber / secondNumber;
+    } else {
+      print("Error: Division by zero is not allowed.");
     }
-    print("Result: $firstNum $operator $secondNum = $result");
+  } else {
+    print("Invalid operator. Please use +, -, *, or /.");
   }
+  print("Result: $firstNumber $operator $secondNumber = ${result}");
 }
